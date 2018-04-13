b892ad00ee20a9a479fa0d5e1a8f862c3bdb34db
Hari Govind <grhari1998@gmail.com>
2018-04-13 00:22:18 -0400

    Made sure the Rook cannot move if there is a pice in the way

--- ChessGame/src/chess/Rook.java
+++ ChessGame/src/chess/Rook.java
@@ -19,9 +19,39 @@ public class Rook extends Piece{
     public boolean isLegalMove(ChessBoard board, Coordinate src, Coordinate dest) {
         int changeInColumn = src.getColumnNumber() - dest.getColumnNumber();
         int changeInRow = src.getRowNumber() - dest.getRowNumber();
-//        int check = 2;
-        if ((changeInColumn == 0 && changeInRow!=0)||changeInRow == 0 && changeInColumn!=0)
-            return super.isLegalMove(board, src, dest);
+
+        if ((changeInColumn == 0 && changeInRow!=0)){
+            int srcY = src.getRowNumber();
+            int destY = dest.getRowNumber();
+            boolean check = true;
+            if (srcY > destY) { // Moving from Black to White. Flip it for uniform iteration
+               int temp = srcY;
+                srcY = destY;
+                destY = temp;   
+            }
+            srcY++; destY--;
+            for (int i=srcY; i<=destY;i++) {
+                if (board.getSquare(new Coordinate(src.getColumnNumber(),i)).isOccupied()) check= false;      
+            }
+            
+            
+            return super.isLegalMove(board, src, dest) && check;}
+        else if (changeInRow == 0 && changeInColumn!=0){
+            int srcX = src.getColumnNumber();
+            int destX = dest.getColumnNumber();
+            boolean check = true;
+            if (srcX > destX) { // Moving from Black to White. Flip it for uniform iteration
+               int temp = srcX;
+                srcX = destX;
+                destX = temp;   
+            }
+            srcX++; destX--;
+            for (int i=srcX; i<=destX;i++) {
+                if (board.getSquare(new Coordinate(i,src.getRowNumber())).isOccupied()) check= false;      
+            }
+            
+            
+            return super.isLegalMove(board, src, dest) && check;}  
         else 
             return false;
         
