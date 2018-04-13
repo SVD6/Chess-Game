fcce1e51b44cb04822e3556411d04a6df1b3adcd
Hari Govind <grhari1998@gmail.com>
2018-04-12 23:54:57 -0400

    Fixed Rook move logic
    Simply checked the change in columns and rows and used switch statements to determine if the move occurred in either the horizontal or vertical direction. Any other movement was considered to be an illegal move and could not proceed.

--- ChessGame/src/chess/Rook.java
+++ ChessGame/src/chess/Rook.java
@@ -17,23 +17,40 @@ public class Rook extends Piece{
     
     @Override
     public boolean isLegalMove(ChessBoard board, Coordinate src, Coordinate dest) {
-        if (!super.isLegalMove(board, src, dest)) return false;
-        // TBD: Can also move along the X axis!!
-        // Only along the COLUMN(Y) axis
-        if (src.getColumn() != dest.getColumn() ) return false;
-        // AND only if intermediate squares are unoccupied
-        int x = src.getColumn();
-        int srcY = src.getRow();
-        int destY = dest.getRow();
-        if (srcY > destY) { // Moving from Black to White. Flip it for uniform iteration
-           int temp = srcY;
-            srcY = destY;
-            destY = temp;   
+        int changeInColumn = src.getColumnNumber() - dest.getColumnNumber();
+        int changeInRow = src.getRowNumber() - dest.getRowNumber();
+        int check = 2;
+        if (changeInColumn == 0 && changeInRow!=0)
+            check = 0;
+        else if(changeInRow == 0 && changeInColumn!=0)
+            check = 1;
+        
+        switch (check){
+            case 0: return super.isLegalMove(board, src, dest);
+                
+            case 1: return super.isLegalMove(board, src, dest);
+            
+            default : return false;
+                
+            
         }
-        srcY++; destY--;
-        for (int i=srcY; i<=destY;i++) {
-            if (board.getSquare(new Coordinate(x,i)).isOccupied()) return false;      
-        }
-        return true;
+//        if (!super.isLegalMove(board, src, dest)) return false;
+//        // TBD: Can also move along the X axis!!
+//        // Only along the COLUMN(Y) axis
+//        if (src.getColumn() != dest.getColumn() ) return false;
+//        // AND only if intermediate squares are unoccupied
+//        int x = src.getColumn();
+//        int srcY = src.getRow();
+//        int destY = dest.getRow();
+//        if (srcY > destY) { // Moving from Black to White. Flip it for uniform iteration
+//           int temp = srcY;
+//            srcY = destY;
+//            destY = temp;   
+//        }
+//        srcY++; destY--;
+//        for (int i=srcY; i<=destY;i++) {
+//            if (board.getSquare(new Coordinate(x,i)).isOccupied()) return false;      
+//        }
+//        return true;
     }
 }
