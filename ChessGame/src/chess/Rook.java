e363d0286c425bcaf5803aaf7ecdcc9e55293e9f
Hari Govind <grhari1998@gmail.com>
2018-04-13 09:52:53 -0400

    Formatting

--- ChessGame/src/chess/Rook.java
+++ ChessGame/src/chess/Rook.java
@@ -4,56 +4,61 @@
  * and open the template in the editor.
  */
 package chess;
-    
+
 /**
  *
  * @author Sai Vikranth Desu
  */
-public class Rook extends Piece{
-    
-    public Rook (ChessColour colour) {
-        super (colour, ChessPieces.ROOK);
+public class Rook extends Piece {
+
+    public Rook(ChessColour colour) {
+        super(colour, ChessPieces.ROOK);
     }
-    
+
     @Override
     public boolean isLegalMove(ChessBoard board, Coordinate src, Coordinate dest) {
         int changeInColumn = src.getColumnNumber() - dest.getColumnNumber();
         int changeInRow = src.getRowNumber() - dest.getRowNumber();
 
-        if ((changeInColumn == 0 && changeInRow!=0)){
+        if ((changeInColumn == 0 && changeInRow != 0)) {
             int srcY = src.getRowNumber();
             int destY = dest.getRowNumber();
             boolean check = true;
             if (srcY > destY) { // Moving from Black to White. Flip it for uniform iteration
-               int temp = srcY;
+                int temp = srcY;
                 srcY = destY;
-                destY = temp;   
+                destY = temp;
             }
-            srcY++; destY--;
-            for (int i=srcY; i<=destY;i++) {
-                if (board.getSquare(new Coordinate(src.getColumnNumber(),i)).isOccupied()) check= false;      
+            srcY++;
+            destY--;
+            for (int i = srcY; i <= destY; i++) {
+                if (board.getSquare(new Coordinate(src.getColumnNumber(), i)).isOccupied()) {
+                    check = false;
+                }
             }
-            
-            
-            return super.isLegalMove(board, src, dest) && check;}
-        else if (changeInRow == 0 && changeInColumn!=0){
+
+            return super.isLegalMove(board, src, dest) && check;
+        } else if (changeInRow == 0 && changeInColumn != 0) {
             int srcX = src.getColumnNumber();
             int destX = dest.getColumnNumber();
             boolean check = true;
             if (srcX > destX) { // Moving from Black to White. Flip it for uniform iteration
-               int temp = srcX;
+                int temp = srcX;
                 srcX = destX;
-                destX = temp;   
+                destX = temp;
             }
-            srcX++; destX--;
-            for (int i=srcX; i<=destX;i++) {
-                if (board.getSquare(new Coordinate(i,src.getRowNumber())).isOccupied()) check= false;      
+            srcX++;
+            destX--;
+            for (int i = srcX; i <= destX; i++) {
+                if (board.getSquare(new Coordinate(i, src.getRowNumber())).isOccupied()) {
+                    check = false;
+                }
             }
-            
-            
-            return super.isLegalMove(board, src, dest) && check;}  
-        else 
+
+            return super.isLegalMove(board, src, dest) && check;
+        } else {
             return false;
-        
+        }
+
     }
 }
